76e274a4d251402c15fb579af3155344f676fc8f
wu <huajun.wu@atrenew.com>
2023-10-24 17:09:12 +0800

    update dockerfile

--- flink-job/src/main/java/com/onepiece/rm/trade/index/calc/function/TxnAmountProcessFunction.java
+++ flink-job/src/main/java/com/onepiece/rm/trade/index/calc/function/TxnAmountProcessFunction.java
@@ -2,19 +2,18 @@ package com.onepiece.rm.trade.index.calc.function;
 
 import com.onepiece.rm.trade.index.calc.model.Transaction;
 import com.onepiece.rm.trade.index.calc.model.result.PanResult;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
 
-@Slf4j
+//@Slf4j
 public class TxnAmountProcessFunction extends ProcessWindowFunction<Transaction, PanResult, String, TimeWindow> {
 
     @Override
     public void process(String key, Context context, Iterable<Transaction> iterable, Collector<PanResult> collector)
         throws Exception {
         Transaction transaction = iterable.iterator().next();
-        log.info("transaction:{}", transaction);
+//        log.info("transaction:{}", transaction);
         PanResult panResult = PanResult.builder()
             .pan(key)
             .last24HourTxnAmount(transaction.getAmount())
--- flink-job/src/main/java/com/onepiece/rm/trade/index/calc/function/TxnCountCollector.java
+++ flink-job/src/main/java/com/onepiece/rm/trade/index/calc/function/TxnCountCollector.java
@@ -1,11 +1,11 @@
 package com.onepiece.rm.trade.index.calc.function;
 
 import com.onepiece.rm.trade.index.calc.model.result.PanResult;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
-@Slf4j
+
+//@Slf4j
 public class TxnCountCollector
     extends ProcessWindowFunction<Long, PanResult, String, TimeWindow> {
 
@@ -15,7 +15,7 @@ public class TxnCountCollector
         final Context context,
         final Iterable<Long> elements,
         final Collector<PanResult> out) throws Exception {
-        log.info("key:{},elements:{}", key, elements);
+//        log.info("key:{},elements:{}", key, elements);
         Long count = elements.iterator().next();
 
         out.collect(PanResult.builder()
